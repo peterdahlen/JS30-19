7673ce6e0dbfff5d502a8e729c68ea04de5ce8ed
Peter Dahlén <peterjandahlen@gmail.com>
2018-03-27 19:53:27 +0200

    function greenScreen

--- scripts.js
+++ scripts.js
@@ -22,7 +22,9 @@ function paintToCanvas() {
         ctx.drawImage(video, 0, 0, width, height);
         let pixels = ctx.getImageData(0, 0, width, height);
         // pixels = redEffect(pixels);
-        pixels = rgbSplit(pixels);
+        // pixels = rgbSplit(pixels);
+        // ctx.globalAlpha = 0.8;
+        pixels = greenScreen(pixels);
         ctx.putImageData(pixels, 0, 0, );
     }, 16);
 }
@@ -56,6 +58,33 @@ function rgbSplit(pixels) {
     }
     return pixels;
 }
+
+function greenScreen(pixels) {
+    const levels = {};
+  
+    document.querySelectorAll('.rgb input').forEach((input) => {
+      levels[input.name] = input.value;
+    });
+  
+    for (i = 0; i < pixels.data.length; i = i + 4) {
+      red = pixels.data[i + 0];
+      green = pixels.data[i + 1];
+      blue = pixels.data[i + 2];
+      alpha = pixels.data[i + 3];
+  
+      if (red >= levels.rmin
+        && green >= levels.gmin
+        && blue >= levels.bmin
+        && red <= levels.rmax
+        && green <= levels.gmax
+        && blue <= levels.bmax) {
+        // take it out!
+        pixels.data[i + 3] = 0;
+      }
+    }
+  
+    return pixels;
+  }
 
 getVideo();
 
