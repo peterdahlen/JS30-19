f678ea566c027059b1d3d2e3ba34aa66f715d4b3
Peter Dahlén <peterjandahlen@gmail.com>
2018-03-27 14:15:57 +0200

    function paintToCanvas()

--- scripts.js
+++ scripts.js
@@ -9,7 +9,18 @@ function getVideo() {
     .then(localMediaStream => { 
         video.src = window.URL.createObjectURL(localMediaStream);
         video.play();
-    });
+    })
+}
+
+function paintToCanvas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+
+    return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height);
+    }, 24);
 }
 
 getVideo();
